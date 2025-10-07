a62c8516aa001fe6211af66c68addadb5c709c20
Maria Luiza <malubd77@gmail.com>
2025-10-07 16:32:03 -0300

    Início do projeto

---
+++ production/src/app/terminal/terminal.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {ProductionControl} from '../production-control/production-control'
+
+@Component({
+  selector: 'app-terminal',
+  imports: [ProductionControl],
+  templateUrl: './terminal.html',
+  styleUrl: './terminal.scss'
+})
+export class Terminal {
+
+}
