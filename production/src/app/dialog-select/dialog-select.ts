d9bd8a569da2be017cee6bd2d470edcee06978cc
Maria Luiza <malubd77@gmail.com>
2025-10-21 16:27:40 -0300

    Adicionando lógica as dailogs

--- production/src/app/dialog-select/dialog-select.ts
+++ production/src/app/dialog-select/dialog-select.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef  } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button'
 
 @Component({
   selector: 'app-dialog-select',
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, MatButtonModule],
   templateUrl: './dialog-select.html',
   styleUrl: './dialog-select.scss'
 })
@@ -13,12 +14,18 @@ export class DialogSelect {
   dialogTitle: string = '';
   optionsList: any = null;
 
+  selectedOptions: any = null
+
   constructor(public dialogRef: MatDialogRef<DialogSelect>, @Inject(MAT_DIALOG_DATA) public data: any){
     this.dialogTitle= data.dialogTitle;
     this.optionsList = data.optionsList;
   }
   
   closeDialog(): void {
-    this.dialogRef.close();
+    this.dialogRef.close(this.selectedOptions);
+  }
+  setSelectedOptions(option: any):void{
+    this.selectedOptions= option; //this= ta dizendo que é nessa classe aqui, que são globais pra ela
+    console.log(':::Selected option:', this.selectedOptions);
   }
 }
--- production/src/app/terminal/terminal.ts
+++ production/src/app/terminal/terminal.ts
@@ -4,30 +4,66 @@ import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dial
 import { DialogSelect } from '../dialog-select/dialog-select';
 
 import productionOrders from '../../assets/files/production-orders.json'
+import stoptypes from '../../assets/files/stop-types.json'
+
 import { CommonModule } from '@angular/common';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+import{ProductionOrder} from '../../../../common/ProductionOrder'
+import{ProductionStatus} from '../../../../common/ProductionStatus'
+import { OrderColors_e, ProductionStatusColor_e, ProductionStatus_e} from '../../../../common/enums/enum';
 
 @Component({
   selector: 'app-terminal',
-  imports: [MatDialogModule, CommonModule],
+  imports: [MatDialogModule, CommonModule, ProductionControl],
   templateUrl: './terminal.html',
   styleUrl: './terminal.scss'
 })
 export class Terminal {
-
   readonly dialog: MatDialog = inject(MatDialog);
 
-  productionOrders: any[] = productionOrders
+  productionOrders: ProductionOrder[] = productionOrders as ProductionOrder[];
+  stopTypes: any = stoptypes
+  productionOrder: ProductionOrder = new ProductionOrder();
+  productionStatus: ProductionStatus = new ProductionStatus();
 
-  async setProductionOrder() {
-    console.log('teste evento de clique')
-    await this.openSelectDiaLog();
+
+  OrderColors_e: typeof OrderColors_e = OrderColors_e
+  ProductionStatus_e: typeof ProductionStatus_e = ProductionStatus_e;
+
+
+  async setProductionOrder(): Promise<void> {
+    const dialogData: object = {
+      dialogTitle: 'Selecionar ordem de produção',
+      optionsList: this.productionOrders
+    }
+
+    const newProductionOrder: ProductionOrder = await this.openSelectDiaLog(dialogData);
+    if(!newProductionOrder) return;
+
+    this.productionOrder = newProductionOrder;
+    this.productionStatus = new ProductionStatus(ProductionStatus_e.InProduction, ProductionStatusColor_e.InProduction);
+    
   }
 
-  openSelectDiaLog(): any{
+  async setStopType(): Promise<void>{
+    const dialogData: object = {
+      dialogTitle: 'Selecionar motivo de parada',
+      optionsList: this.stopTypes
+    }
+    const stopType = await this.openSelectDiaLog(dialogData)
+    if(!stopType) return
+
+    this.productionStatus = {
+      color: ProductionStatusColor_e.Stop,
+      status: ProductionStatus_e[stopType.value as keyof typeof ProductionStatus_e]
+    }
+  }
+
+  openSelectDiaLog(dialogData: any): Promise<ProductionOrder>{
     const dialogRef: MatDialogRef<DialogSelect, any> = this.dialog.open(DialogSelect, {
       width: '950px',
       panelClass: 'custom-dialog',
-      data: {dialogTitle: 'Teste dialog', optionsList: this.productionOrders}
+      data: dialogData
     });
 
     return new Promise((resolve) => {
